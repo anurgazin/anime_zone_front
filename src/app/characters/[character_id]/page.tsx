300788cc34f7ef2ad4ab63beeaab9e0863bf53bb
anurgazin <dindog24@gmail.com>
2024-11-02 15:27:01 -0400

    Comment and Character lists fetch for Character Display Single added

--- src/app/characters/[character_id]/page.tsx
+++ src/app/characters/[character_id]/page.tsx
@@ -1,22 +1,28 @@
 "use client"
 import { useEffect, useState } from "react";
 import DisplaySingle from "@/components/characters/displaySingle";
-import { getSingleCharacter } from "@/lib/api";
-import { CharacterAPI } from "@/lib/types";
+import { getAllCharacterListsByCharacterId, getAllComments, getSingleCharacter } from "@/lib/api";
+import { CharacterAPI, CharacterList, Comment } from "@/lib/types";
 import Loading from "@/components/loading";
 import Error from "@/components/error";
 
 export default function SingleCharacter({ params }: { params: { character_id: string } }) {
     const character_id = params.character_id;
     const [character, setCharacter] = useState<CharacterAPI>();
+    const [comments, setComments] = useState<Comment[]>();
+    const [lists, setLists] = useState<CharacterList[]>();
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string>("");
 
     useEffect(() => {
         const fetchCharacter = async () => {
             try {
-                const response = await getSingleCharacter(character_id); // Fetch single character data by ID
-                setCharacter(response.data); // Store the fetched anime data
+                const character_response = await getSingleCharacter(character_id); // Fetch single character data by ID
+                const comment_response = await getAllComments("character", character_id)
+                const list_response = await getAllCharacterListsByCharacterId(character_id)
+                setCharacter(character_response.data);
+                setComments(comment_response.data);
+                setLists(list_response.data)
                 setLoading(false); // Set loading to false once data is fetched
             } catch (err) {
                 setError("Failed to fetch character.");
@@ -33,7 +39,7 @@ export default function SingleCharacter({ params }: { params: { character_id: st
     if (character) {
         return (
             <div>
-                <DisplaySingle character={character} />
+                <DisplaySingle character={character} comments={comments} lists={lists} />
             </div>
         );
     }
---
+++ src/components/lists/characterList/index.tsx
@@ -0,0 +1,24 @@
+import { CharacterList } from "@/lib/types";
+type CharacterListProps = {
+    characterList: CharacterList;
+}
+
+export default function CharacterListDisplay({ characterList }: CharacterListProps) {
+    // Rating color based on its value
+    const rating_color = characterList.rating > 0
+        ? "text-green-500"
+        : characterList.rating < 0
+            ? "text-red-500"
+            : "text-gray-500";
+    return (
+        <div className="pb-4 mb-4">
+            {/* Header with List Name */}
+            <div className="flex justify-between items-center">
+                <p className="font-semibold text-gray-800">{characterList.name}</p>
+                <p className="text-sm text-gray-500">{characterList.user.username}</p>
+            </div>
+            {/* Rating */}
+            <p className={`font-semibold ${rating_color}`}>{characterList.rating}</p>
+        </div>
+    );
+};
