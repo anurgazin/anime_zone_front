3cd397caf49c7d0f867c970e304041f810b38c08
anurgazin <dindog24@gmail.com>
2024-11-08 17:52:54 -0500

    Layout in Anime Display Single changed(Grid, Media Display and etc.)

--- src/components/anime/displaySingle/index.tsx
+++ src/components/anime/displaySingle/index.tsx
@@ -36,64 +36,69 @@ export default function DisplaySingle({ anime, comments, rating, animeList, char
     };
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-5 grid-rows-auto gap-4">
+        <div className="grid grid-cols-1 grid-rows-auto gap-4">
             {/* Top Banner */}
-            <div className="lg:col-span-5 relative overflow-hidden -z-10 h-48 sm:h-56">
+            <div className="relative overflow-hidden -z-10 h-48 sm:h-56">
                 <Image src={anime.logo} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill={true} className="blur-sm object-cover" alt="Anime Banner" />
             </div>
 
-            {/* Information */}
-            <div className="lg:col-span-5 grid grid-cols-1 lg:grid-cols-6 lg:grid-rows-auto gap-5 justify-self-center px-4 sm:px-10 py-4">
-                {/* Logo section */}
-                <div className="flex justify-center lg:justify-start relative shadow-md bg-white w-full h-[350px]"
-                    onClick={() => handleImageClick({
-                        src: anime.logo,
-                        width: 0,
-                        height: 0
-                    })}
-                >
-                    <Image src={anime.logo} priority fill={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="rounded-lg lg:shadow-lg object-contain lg:object-fill lg:hover:scale-[1.10] transition-scale duration-200" alt="Anime Logo" />
-                </div>
-
-                {/* Data info */}
-                <div className="lg:col-span-3 w-full lg:row-span-1 p-4 rounded-lg border-2 border-orange-200 lg:border-none">
-                    <h1 className="text-2xl sm:text-3xl font-anton text-gray-800 mb-4">{anime.title}</h1>
-                    <div className="text-lg sm:text-xl text-gray-600 grid grid-cols-1 sm:grid-cols-2 gap-4 lg:border-2 lg:border-orange-200 shadow-md bg-white p-2 rounded-lg">
-                        <div>
-                            <p><span className="font-semibold">Rating:</span> {anime.average_rating > 0 ? `${anime.average_rating} / 10` : "Not Available"}</p>
-                            <p><span className="font-semibold">Studio:</span> {studio}</p>
-                            <p><span className="font-semibold">Type:</span> {anime.type.toUpperCase()}</p>
-                            <p><span className="font-semibold">Genres:</span> {genre}</p>
-                            <p><span className="font-semibold">ESRB:</span> {anime.esrb}</p>
+            {/* Main Info */}
+            <div className='flex flex-col gap-4 pt-1 p-6'>
+                <div className='grid grid-cols-1 lg:grid-cols-10 gap-8'>
+                    {/* Logo */}
+                    <div className='lg:col-span-2 flex justify-center'>
+                        <div className="relative w-full h-[350px]"
+                            onClick={() => handleImageClick({
+                                src: anime.logo,
+                                width: 0,
+                                height: 0
+                            })}
+                        >
+                            <Image src={anime.logo} priority fill={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="rounded-lg object-contain lg:hover:scale-[1.025] transition-scale duration-200" alt="Anime Logo" />
                         </div>
-                        <div>
-                            <p><span className="font-semibold">Release Date:</span> {new Date(anime.release_date).toLocaleDateString()}</p>
-                            <p><span className="font-semibold">Status:</span> {anime.status.toUpperCase()}</p>
-                            <p><span className="font-semibold">Number of Episodes:</span> {anime.episodes}</p>
-                            <p><span className="font-semibold">Duration:</span> {anime.duration} mins</p>
+                    </div>
+                    {/* Data info */}
+                    <div className="lg:col-span-8 p-4 rounded-lg border-2 border-orange-200 lg:border-none">
+                        <h1 className="text-2xl sm:text-3xl font-anton text-gray-800 mb-4">{anime.title}</h1>
+                        <div className="text-lg sm:text-xl text-gray-600 grid grid-cols-1 sm:grid-cols-2 gap-4 lg:border-2 lg:border-orange-200 shadow-md bg-white p-2 rounded-lg">
+                            <div>
+                                <p><span className="font-semibold">Rating:</span> {anime.average_rating > 0 ? `${anime.average_rating} / 10` : "Not Available"}</p>
+                                <p><span className="font-semibold">Studio:</span> {studio}</p>
+                                <p><span className="font-semibold">Type:</span> {anime.type.toUpperCase()}</p>
+                                <p><span className="font-semibold">Genres:</span> {genre}</p>
+                                <p><span className="font-semibold">ESRB:</span> {anime.esrb}</p>
+                            </div>
+                            <div>
+                                <p><span className="font-semibold">Release Date:</span> {new Date(anime.release_date).toLocaleDateString()}</p>
+                                <p><span className="font-semibold">Status:</span> {anime.status.toUpperCase()}</p>
+                                <p><span className="font-semibold">Number of Episodes:</span> {anime.episodes}</p>
+                                <p><span className="font-semibold">Duration:</span> {anime.duration} mins</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                {/* Media Section */}
-                <div className="lg:col-span-2 lg:row-span-2 flex flex-col">
-                    <h2 className="text-xl sm:text-2xl font-anton text-gray-800 mb-2">Media</h2>
-                    <div className="grid grid-cols-subgrid space-y-2">
+                {/* Media */}
+                <div className="border-2 border-orange-200 p-6 rounded-lg shadow-md">
+                    <h2 className="text-xl sm:text-2xl font-anton text-gray-800 mb-2 underline decoration-orange-300 underline-offset-8">
+                        Media
+                    </h2>
+                    <div className="flex flex-row flex-wrap gap-4 justify-center">
                         {anime.media.map((mediaItem, i) => {
                             const isYouTubeLink = mediaItem.includes("youtube.com") || mediaItem.includes("youtu.be");
 
                             return (
-                                <div key={i} className="col-start-2">
+                                <div key={i}>
                                     {isYouTubeLink ? (
                                         // Render YouTube iframe for YouTube links
                                         <iframe
-                                            src={mediaItem.includes("youtu.be")
-                                                ? mediaItem.replace("youtu.be/", "www.youtube.com/embed/")
-                                                : mediaItem.replace("watch?v=", "embed/")
+                                            src={
+                                                mediaItem.includes("youtu.be")
+                                                    ? mediaItem.replace("youtu.be/", "www.youtube.com/embed/")
+                                                    : mediaItem.replace("watch?v=", "embed/")
                                             }
                                             width="320"
-                                            height="170"
-                                            className="rounded-md shadow-md border-2 border-orange-200 hover:scale-[1.05] transition-scale duration-200"
+                                            height="180"
+                                            className="rounded-md shadow-md border-2 border-orange-200 hover:scale-[1.025] transition-transform duration-200"
                                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                             allowFullScreen
                                             title={`YouTube video ${i}`}
@@ -101,17 +106,20 @@ export default function DisplaySingle({ anime, comments, rating, animeList, char
                                     ) : (
                                         // Render image for non-YouTube links
                                         <div
-                                            onClick={() => handleImageClick({
-                                                src: mediaItem,
-                                                width: 320,
-                                                height: 240
-                                            })}
-                                            className='relative w-[320px] h-[170px]'>
+                                            onClick={() =>
+                                                handleImageClick({
+                                                    src: mediaItem,
+                                                    width: 320,
+                                                    height: 240,
+                                                })
+                                            }
+                                            className="relative w-[320px] h-[180px]">
                                             <Image
                                                 src={mediaItem}
                                                 fill
+                                                quality={100}
                                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                                className="rounded-md object-cover shadow-md border-2 border-orange-200 lg:hover:scale-[1.10] transition-scale duration-200"
+                                                className="rounded-md object-cover hover:scale-[1.025] transition-transform duration-200"
                                                 alt={`Media ${i}`}
                                             />
                                         </div>
@@ -123,15 +131,14 @@ export default function DisplaySingle({ anime, comments, rating, animeList, char
                 </div>
 
                 {/* Description */}
-                <div className="lg:col-span-4 lg:row-start-2 border-2 border-orange-200 p-4 shadow-md bg-white rounded-lg">
+                <div className='border-2 border-orange-200 p-4 shadow-md bg-white rounded-lg'>
                     <h2 className="text-xl sm:text-2xl font-anton text-gray-800 mb-2 underline decoration-orange-300 underline-offset-8">Description</h2>
                     <p className="text-gray-700 whitespace-pre-wrap">{anime.description}</p>
                 </div>
-
                 {/* Characters */}
-                <div className="lg:col-span-6 lg:row-start-3 border-2 border-orange-200 p-6 rounded-lg shadow-md bg-white">
+                <div className="border-2 border-orange-200 p-6 rounded-lg shadow-md bg-white">
                     <h2 className="text-xl sm:text-2xl font-anton text-gray-800 mb-6 underline decoration-orange-300 underline-offset-4">Characters</h2>
-                    <div className="flex flex-row flex-wrap gap-6 items-center ">
+                    <div className="flex flex-row flex-wrap gap-4 items-center">
                         {characters && characters.length > 0 ? (
                             characters.map((c, i) => (
                                 <CharacterFromAnimeCard key={i} character={c} />
@@ -142,13 +149,11 @@ export default function DisplaySingle({ anime, comments, rating, animeList, char
                     </div>
                 </div>
 
-
                 {/* Reviews */}
-                <div className="lg:col-span-6 lg:row-start-4 border-2 border-orange-200 shadow-md bg-white p-4 rounded-lg">
+                <div className="border-2 border-orange-200 shadow-md bg-white p-4 rounded-lg">
                     <h2 className="text-xl sm:text-2xl font-anton text-gray-800 mb-4 underline decoration-orange-300 underline-offset-8">Reviews</h2>
                     {rating && rating.length > 0 ? (
                         rating
-                            .filter(r => r.review)
                             .map((r, i) => (
                                 <RatingComponent key={i} rating={r} />
                             ))
@@ -158,7 +163,7 @@ export default function DisplaySingle({ anime, comments, rating, animeList, char
                 </div>
 
                 {/* Comments and Lists Section */}
-                <div className="lg:col-span-6 lg:row-start-5 grid grid-cols-1 lg:grid-cols-2 grid-rows-auto gap-2">
+                <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-auto gap-2">
                     {/* Comments */}
                     <div className="border-2 border-orange-200 p-4 shadow-md bg-white rounded-lg space-y-4">
                         <h2 className="text-xl sm:text-2xl font-anton text-gray-800 mb-4 underline decoration-orange-300 underline-offset-8">Comments</h2>
